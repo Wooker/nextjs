57d10a9c83a16d5725f05ff50380b81c8a907bc6
Zakhar Semenov <deadlion24@gmail.com>
2021-10-29 18:50:57 +0600

    fix errors

--- components/layout.js
+++ components/layout.js
@@ -52,6 +52,42 @@ export default function Layout({children, home}) {
 				<li className="nav-item"><a href="#" className="nav-link h5">О нас</a></li>
 			</ul>
 		</header>
+<article class="my-3" id="carousel">
+      <div class="bd-heading sticky-xl-top align-self-start mt-5 mb-3 mt-xl-0 mb-xl-2">
+        <h3>Carousel</h3>
+        <a class="d-flex align-items-center" href="/docs/5.1/components/carousel/">Documentation</a>
+      </div>
+
+      <div>
+        <div class="bd-example">
+        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-indicators align-items-center">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2" class=""></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3" class="active" aria-current="true"></button>
+          </div>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Third slide" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#555"></rect><text x="50%" y="50%" fill="#333" dy=".3em">Third slide</text></svg>
+
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Third slide label</h5>
+                <p>Some representative placeholder content for the third slide.</p>
+              </div>
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+        </div>
+      </div>
+    </article>
 
       <main>{children}</main>
 
--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,6 @@ export default function Home() {
 				</>
 			) : (
 				<>
-				<Entries entries={prods.entries.products.edges} />
 				</>
 			)}
 		</Layout>
